e65578a6aafd70f9572aafe9ae70299b792de37d
Sebaslc1077 <32956259+Sebaslc1077@users.noreply.github.com>
2018-01-12 21:56:15 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -135,3 +135,14 @@ function exittoabout(){
 		stop();
 	} 
 }
+
+function myFunction() {
+    var txt;
+    if (confirm("Press a button!") == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
+
